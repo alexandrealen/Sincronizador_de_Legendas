4932a810e10f0d65607d61221fb8cecadbaf2782
raviston <alexandrealen@hotmail.com>
2021-03-09 20:37:21 -0300

    Inicio do projeto, criado com o vs

---
+++ Sincronizador_de_legendas/Sincronizador_de_legendas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sincronizador_de_legendas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
