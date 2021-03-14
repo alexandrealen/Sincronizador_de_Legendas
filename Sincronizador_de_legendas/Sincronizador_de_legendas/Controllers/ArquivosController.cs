f5b6b7e1ecfb84625aaaaf948e99073799ecfac1
raviston <alexandrealen@hotmail.com>
2021-03-13 21:42:58 -0300

    Adiciona opção de download

---
+++ Sincronizador_de_legendas/Sincronizador_de_legendas/Controllers/ArquivosController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sincronizador_de_legendas.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Sincronizador_de_legendas.Controllers
+{
+    public class ArquivosController : Controller
+    {
+        IWebHostEnvironment _appEnvironment;
+        ModelArquivos oModelArquivos = new ModelArquivos();
+        //criei esse construtor para conseguir pegar o caminho onde a aplicação está rodando 
+        //e posteriormente salvar os arquivos na pasta resources
+        public ArquivosController(IWebHostEnvironment env)
+        {
+            _appEnvironment = env;
+        }
+
+        // GET: /Reports/
+        public ActionResult Index()
+        {
+            var _arquivos = oModelArquivos.GetArquivos($"{_appEnvironment.ContentRootPath}/Resources\\");
+            return View(_arquivos);
+        }
+
+        public FileResult Download(string id)
+        {
+            int _arquivoId = Convert.ToInt32(id);
+            var arquivos = oModelArquivos.GetArquivos($"{_appEnvironment.ContentRootPath}/Resources\\");
+
+            string nomeArquivo = (from arquivo in arquivos
+                                  where arquivo.ID == _arquivoId
+                                  select arquivo.Caminho).First();
+
+            string contentType = "text/srt";
+            return File(nomeArquivo, contentType, $"Subtitle.srt");
+        }
+    }
+}
---
+++ Sincronizador_de_legendas/Sincronizador_de_legendas/Models/ModelArquivos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Sincronizador_de_legendas.Models
+{
+    public class ModelArquivos
+    {
+        public List<Arquivos> GetArquivos(string path)
+        {
+            List<Arquivos> lstArquivos = new List<Arquivos>();
+            //DirectoryInfo dirInfo = new DirectoryInfo(HostingEnvironment.MapPath("~/Arquivos"));
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            int i = 0;
+            foreach (var item in dirInfo.GetFiles())
+            {
+                lstArquivos.Add(new Arquivos()
+                {
+                    ID = i + 1,
+                    Nome = item.Name,
+                    Caminho = dirInfo.FullName + item.Name
+                });
+                i = i + 1;
+            }
+            return lstArquivos;
+        }
+    }
+}
