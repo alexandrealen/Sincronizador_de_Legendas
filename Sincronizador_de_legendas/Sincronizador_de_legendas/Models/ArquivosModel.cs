cd5ef8513a8b690396b81ec0b604259801392d3a
alexandre_alencar <alexandre.alencar@intelitrader.com.br>
2021-03-14 20:49:18 -0300

    remove partes inutilizadas do codigo

--- Sincronizador_de_legendas/Sincronizador_de_legendas/Models/ArquivosModel.cs
+++ Sincronizador_de_legendas/Sincronizador_de_legendas/Models/ArquivosModel.cs
@@ -6,20 +6,16 @@ namespace Sincronizador_de_legendas.Models
 {
     public class ArquivosModel
     {
-        public int ID { get; set; }
         public string Nome { get; set; }
         public string Caminho { get; set; }
         public static IEnumerable<ArquivosModel> GetArquivos(string path)
         {
             List<ArquivosModel> lstArquivos = new List<ArquivosModel>();
             DirectoryInfo dirInfo = new DirectoryInfo(path);
-            int i = 0;
             foreach (var item in dirInfo.GetFiles().OrderBy(x => x.CreationTime))
             {
-                i++;
                 lstArquivos.Add(new ArquivosModel()
                 {
-                    ID = i,
                     Nome = item.Name,
                     Caminho = dirInfo.FullName + item.Name
                 });
